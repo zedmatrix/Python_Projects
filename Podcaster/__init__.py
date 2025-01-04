efefe83b09c22590897cb250e7323ff086237ddb
zedmatrix <zedmatrix007@gmail.com>
2025-01-04 00:00:59 -0800

    Create __init__.py

---
+++ Podcaster/__init__.py
@@ -0,0 +1,19 @@
+ Requires Dictionary List of Podcast Entries
+ PODCASTS = {"key": {"name": "", "day": "Sunday", "url":"https://"}}
+
+"""
+
+Usage = { "message": "No action specified. Please Use:\n"
+    "--list to list podcasts\n"
+    "--podcast 'podcast' to select one.\n"
+}
+
+# URL constants
+PODCASTS = {
+    "dtns": {"name":"Daily Tech News Show", "day":" Anyday", "url":"http://feeds.feedburner.com/daily_tech_news_show"},
+}
+
+OLD_PODCASTS = {
+    "lol": {"name":"Linux Out Loud", "day":" Sunday", "url":"https://dlnxtend.com/rss"},
+}
+    
