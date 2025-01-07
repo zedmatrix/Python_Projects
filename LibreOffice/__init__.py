cf71d16cd8bd7c78ca74014e5f8d75a538e90013
zedmatrix <zedmatrix007@gmail.com>
2025-01-06 21:31:32 -0800

    Update __init__.py

--- LibreOffice/__init__.py
+++ LibreOffice/__init__.py
@@ -4,9 +4,8 @@ Contains constants, essential imports, and the connection function for LibreOffi
 """
 
 import uno
-from LibreOffice.connect_libre import connect_libre
+from LibreOffice.connect_libre import connect_libre, open_office
 from LibreOffice.document_info import set_document_info, get_document_info
 from LibreOffice.document_class import MyDocClass
 
-
-__all__ = ['connect_libre', 'set_document_info', 'get_document_info', 'MyDocClass']
+__all__ = ['connect_libre', 'open_office', 'set_document_info', 'get_document_info', 'MyDocClass']
