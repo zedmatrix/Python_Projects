9131c0ef1c771bb4fde2e201a22d54147a17505c
zedmatrix <zedmatrix007@gmail.com>
2025-01-06 21:21:11 -0800

    Update cell_functions.py

--- LibreOffice/Budget/cell_functions.py
+++ LibreOffice/Budget/cell_functions.py
@@ -12,3 +12,29 @@ def MergeCells(sheet, col_left: str, start_row, col_right: str, end_row):
     cell_range = sheet.getCellRangeByPosition(start_col, start_row, end_col, end_row)
     cell_range.merge(False)
     cell_range.merge(True)
+
+def CellHeight(sheet, row: int, height=300, optimal=True):
+    try:
+        row_index = row - 1
+        row_object = sheet.Rows[row_index]
+        if optimal:
+            row_object.OptimalWidth = True
+            print(f"Set row {row} to optimal height.")
+        else:
+            row_object.Height = height
+            print(f"Set height of row {row} to {height}.")
+    except Exception as e:
+        print(f"Error setting height for row {row}: {e}")
+
+def CellWidth(sheet, col: str, width=600, optimal=True):
+    try:
+        columns = sheet.getColumns()
+        column = columns.getByName(col)
+        if optimal:
+            column.OptimalWidth = True
+            print(f"Set column {col} to optimal width.")
+        else:
+            column.Width = width
+            print(f"Set width of column {col} to {width}.")
+    except Exception as e:
+        print(f"Error setting width for column {col}: {e}")
