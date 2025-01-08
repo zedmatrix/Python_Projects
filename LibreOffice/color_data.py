b86b06b25276872108e26ba5dd3d25de8d90d926
zedmatrix <zedmatrix007@gmail.com>
2025-01-08 12:32:48 -0800

    Add files via upload

---
+++ LibreOffice/color_data.py
@@ -0,0 +1,38 @@
+from typing import Final
+
+# Color constants
+dark_green: Final[int] = 0x027002
+light_green: Final[int] = 0x97ff97
+dark_red: Final[int] = 0xb60000
+light_red: Final[int] = 0xffa2a2
+light_grey: Final[int] = 0xeeeeee
+dark_grey: Final[int] = 0x404040
+white: Final[int] = 0xffffff
+black: Final[int] = 0x000000
+
+bright_orange: Final[int] = 0xf97f05
+
+# New color pairings
+dark_blue: Final[int] = 0x00008b
+light_blue: Final[int] = 0xadd8e6
+dark_yellow: Final[int] = 0xffd700
+light_yellow: Final[int] = 0xffffe0
+dark_cyan: Final[int] = 0x008b8b
+light_cyan: Final[int] = 0xc6e6e6
+dark_orange: Final[int] = 0xff8c00
+light_orange: Final[int] = 0xffe5b4
+dark_magenta: Final[int] = 0x8b008b
+light_magenta: Final[int] = 0xff77ff
+
+__all__ = [
+    'dark_green', 'light_green',
+    'dark_red', 'light_red',
+    'dark_grey', 'light_grey',
+    'white', 'black',
+    'dark_blue', 'light_blue',
+    'dark_yellow', 'light_yellow',
+    'dark_cyan', 'light_cyan',
+    'dark_orange', 'light_orange',
+    'dark_magenta', 'light_magenta',
+    'bright_orange'
+]
